09152e859dc28190eef0e375f3b1f5655c201d27
Tamim khan <mdtamimkhan842@gmail.com>
2025-02-25 02:33:15 +0600

    update my portfolio

--- app.js
+++ app.js
@@ -17,3 +17,45 @@ resumebtn.forEach((btn, idx) => {
         resumedetails[idx].classList.add('active');
     });
 });
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+const totalSlides = document.querySelectorAll('.portfolio-carousel .img-item').length;
+
+
+const activePortfolio = () => {
+    const ImaSlide = document.querySelector('.portfolio-carousel');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+    ImaSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active');
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < totalSlides - 1) {
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else {
+        index = 0;
+        arrowRight.classList.add('disabled');
+    }
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else {
+        index = totalSlides - 1;
+        arrowLeft.classList.add('disabled');
+    }
+    activePortfolio();
+});
