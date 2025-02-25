42ecf17cc5ec06c8fafdd8159c76ef201e7c0be6
Tamim khan <mdtamimkhan842@gmail.com>
2025-02-26 02:43:16 +0600

    update responsive my page

--- app.js
+++ app.js
@@ -1,40 +1,66 @@
-//active my others services not only home
 const navLinks = document.querySelectorAll('header nav a');
+const section = document.querySelectorAll('section');
 const logoLink = document.querySelector('.logo');
 
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
+
 const activePage = () => {
+    const barsBox = document.querySelector('.bars-box');
+    const header = document.querySelector('header');
+
+    header.classList.remove('active');
+    setTimeout(() => {
+        header.classList.add('active');
+    }, 1100);
+
     navLinks.forEach(link => {
         link.classList.remove('active');
     });
-}
+
+    barsBox.classList.remove('active');
+    setTimeout(() => {
+        barsBox.classList.add('active');
+    }, 1100);
+
+    section.forEach(section => {
+        section.classList.remove('active');
+    });
+};
 
 navLinks.forEach((link, idx) => {
     link.addEventListener('click', () => {
         if (!link.classList.contains('active')) {
             activePage();
-
             link.classList.add('active');
+
+            setTimeout(() => {
+                section[idx].classList.add('active');
+            }, 1100);
         }
     });
 });
 
 logoLink.addEventListener('click', () => {
-    //The contains() method returns true if a node is a descendant of a node.
-    //The contains() method returns false if not.
-
     if (!navLinks[0].classList.contains('active')) {
         activePage();
-
         navLinks[0].classList.add('active');
+
+        setTimeout(() => {
+            section[0].classList.add('active');
+        }, 1100);
     }
 });
 
 const resumebtn = document.querySelectorAll('.resume-btn');
 
-//btn means resume-btn ar value gula, mention here
 resumebtn.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
-
         const resumedetails = document.querySelectorAll('.resume-detail');
 
         resumebtn.forEach(btn => {
@@ -55,26 +81,24 @@ const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left
 let index = 0;
 const totalSlides = document.querySelectorAll('.portfolio-carousel .img-item').length;
 
-
 const activePortfolio = () => {
     const ImaSlide = document.querySelector('.portfolio-carousel');
     ImaSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
     const portfolioDetails = document.querySelectorAll('.portfolio-detail');
 
     portfolioDetails.forEach(detail => {
         detail.classList.remove('active');
     });
     portfolioDetails[index].classList.add('active');
-}
+};
 
 arrowRight.addEventListener('click', () => {
     if (index < totalSlides - 1) {
         index++;
         arrowLeft.classList.remove('disabled');
-    }
-    else {
+    } else {
         index = 0;
-        arrowRight.classList.add('disabled');
     }
     activePortfolio();
 });
@@ -83,10 +107,8 @@ arrowLeft.addEventListener('click', () => {
     if (index > 0) {
         index--;
         arrowRight.classList.remove('disabled');
-    }
-    else {
+    } else {
         index = totalSlides - 1;
-        arrowLeft.classList.add('disabled');
     }
     activePortfolio();
 });
